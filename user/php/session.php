d94486366df19c0b997c7566ae1ea1b7491db2cb
gabriel7759 <gabriel7759@hotmail.com>
2018-01-16 23:09:25 -0800

    setting up connection to database

--- user/php/session.php
+++ user/php/session.php
@@ -1,9 +1,9 @@
 <?php
-//$mysqli = new mysqli("db688560822.db.1and1.com", "dbo688560822", "charlo1357", "db688560822");
+$mysqli = new mysqli("db719532687.db.1and1.com", "dbo719532687", "flako1979", "db719532687");
 
 //$mysqli = new mysqli("localhost", "adminMomentos", "adminMomentos", "ColgateClub");
 //$mysqli = new mysqli("localhost", "root", "", "bussoly");
-$mysqli = new mysqli("localhost", "user", "password", "colgate");
+//$mysqli = new mysqli("localhost", "user", "password", "colgate");
 if ($mysqli->connect_errno) {
     echo "Error 504 : (" . $mysqli->connect_errno . ")" . $mysqli->connect_error;
 }
